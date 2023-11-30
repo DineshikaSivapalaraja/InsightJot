e7579949bd2f825b2e0d855edb719b18bcccad7f
Dineshika <99dinuraja@gmail.com>
2023-11-30 19:16:51 +0530

    applied react router link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
+import NavBar from './NavBar'
 import Home from './pages/Home';
 import About from './pages/About';
 import Article from './pages/Article';
@@ -11,14 +11,15 @@ function App() {
   return (
     <BrowserRouter>
     <div className="App">
-      <h1>My Blog</h1>
+      <NavBar />
       <div id="page-body">
         <Routes>
+          {/* navigate pages separately for different pages*/} 
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About />} /> {/* navigate about page*  */} 
           <Route path="/articles" element={<Article />} />
-          <Route path="/articles/:articleId" element={<ArticleList />} />
-          My blog app
+          <Route path="/articles/:articleId" element={<ArticleList />} />{/* navigate articlelist page  http://localhost:3005/articles/artcleId */}
+        My blog app
         </Routes>  
       </div>
     </div>
